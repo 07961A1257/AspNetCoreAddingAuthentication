59ba0724e95f3fd2de6bbd9e3e5d4169f620f03f
07961A1257 <yvakchaitanya@gmail.com>
2022-08-06 17:21:18 +0530

    Create Constructor For AccountController

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -9,6 +9,14 @@ namespace WishList.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
 
         public IActionResult Index()
         {
