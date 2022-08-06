bfb011a161777053ea5cbaf9a44ad203d17bdf21
07961A1257 <yvakchaitanya@gmail.com>
2022-08-06 17:19:48 +0530

    Create Private Fields For AccountController

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
         public IActionResult Index()
         {
             return View();
