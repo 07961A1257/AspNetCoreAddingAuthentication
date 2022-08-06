d36e73372c9a4bfd863963b212ef7efbb21e000f
07961A1257 <yvakchaitanya@gmail.com>
2022-08-06 18:17:04 +0530

    Update AccountController.cs

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -84,6 +84,7 @@ namespace WishList.Controllers
         public IActionResult Logout()
         {
             _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
